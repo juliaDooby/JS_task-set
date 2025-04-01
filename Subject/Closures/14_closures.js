4b1145cf9bd20a09c5c656f3d5b89a32526cbbcf
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-01 22:53:26 +0300

    Update 14_closures.js

--- Subject/Closures/14_closures.js
+++ Subject/Closures/14_closures.js
@@ -1,1 +1,32 @@
+---------------------------------------------------------------------------------------------
+⋙ ❍ Замыкания:
+--------------- 
+⌛ Задача 14: Фабрика событий
+Напишите функцию `createEventEmitter`, которая позволяет подписываться на события и вызывать их.
+🎯 Решение:
+```javascript
+function createEventEmitter() {
+  const events = {};
 
+  return {
+    on: function (event, listener) {
+      if (!events[event]) {
+        events[event] = [];
+      }
+      events[event].push(listener);
+    },
+    emit: function (event, ...args) {
+      if (events[event]) {
+        events[event].forEach(listener => listener(...args));
+      }
+    }
+  };
+}
+
+const emitter = createEventEmitter();
+emitter.on('sayHello', name => console.log(`Hello, ${name}!`));
+emitter.emit('sayHello', 'Alice'); // Hello, Alice!
+```
+Объяснение:
+- Замыкание сохраняет объект `events`, который хранит подписки на события.
+---------------------------------------------------------------------------------------------
