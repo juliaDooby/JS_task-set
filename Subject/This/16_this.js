4638cb44c474fc2adc458b9bd625dcb62d78da6c
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-01 16:18:08 +0300

    Update 16_this.js

--- Subject/This/16_this.js
+++ Subject/This/16_this.js
@@ -1,1 +1,23 @@
-
+---------------------------------------------------------------------------------------------
+⋙ ❍ Работа с this:
+---
+16. this в EventListener
+⌛ Задача: Как вывести `"Frontend"` при клике?
+```javascript
+const obj = {
+  name: "Frontend",
+  init: function() {
+    document.body.addEventListener("click", function() {
+      console.log(this.name);
+    });
+  }
+};
+obj.init();
+```
+🎯 Решение: Используйте стрелочную функцию:
+```javascript
+document.body.addEventListener("click", () => {
+  console.log(this.name);
+});
+```
+---------------------------------------------------------------------------------------------
