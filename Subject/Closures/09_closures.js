569a49ef0705952e794bb1960b76338e81daa656
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-01 22:51:43 +0300

    Update 09_closures.js

--- Subject/Closures/09_closures.js
+++ Subject/Closures/09_closures.js
@@ -1,1 +1,43 @@
+---------------------------------------------------------------------------------------------
+⋙ ❍ Замыкания:
+---------------
+⌛ Задача 9: Таймер с паузой
+Напишите функцию `createTimer`, которая позволяет запускать таймер, останавливать его и получать текущее значение.
+🎯 Решение:
+```javascript
+function createTimer() {
+  let startTime = 0;
+  let elapsedTime = 0;
+  let timer;
 
+  return {
+    start: function () {
+      if (!timer) {
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(() => {
+          elapsedTime = Date.now() - startTime;
+        }, 100);
+      }
+    },
+    stop: function () {
+      if (timer) {
+        clearInterval(timer);
+        timer = null;
+      }
+    },
+    getTime: function () {
+      return (elapsedTime / 1000).toFixed(2) + 's';
+    }
+  };
+}
+
+const timer = createTimer();
+timer.start();
+setTimeout(() => {
+  timer.stop();
+  console.log(timer.getTime()); // ~3.00s
+}, 3000);
+```
+Объяснение:
+- Замыкание сохраняет переменные `startTime`, `elapsedTime` и `timer`, позволяя манипулировать состоянием таймера.
+---------------------------------------------------------------------------------------------
