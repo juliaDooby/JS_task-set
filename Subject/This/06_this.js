37c8a4a0ac41db9b39ef706500796ea77a598bf7
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-01 16:14:58 +0300

    Update 06_this.js

--- Subject/This/06_this.js
+++ Subject/This/06_this.js
@@ -1,1 +1,29 @@
-
+---------------------------------------------------------------------------------------------
+⋙ ❍ Работа с this:
+---
+6. Потеря контекста в обработчике событий
+⌛ Задача: Как сделать так, чтобы код вывел `"Frontend"`?
+```javascript
+const obj = {
+  name: "Frontend",
+  showThis: function() {
+    document.addEventListener("click", function() {
+      console.log(this.name);
+    });
+  }
+};
+obj.showThis();
+```
+🎯 Решение: Используйте стрелочную функцию:
+```javascript
+document.addEventListener("click", () => {
+  console.log(this.name);
+});
+```
+Или `bind`:
+```javascript
+document.addEventListener("click", function() {
+  console.log(this.name);
+}.bind(obj));
+```
+---------------------------------------------------------------------------------------------
