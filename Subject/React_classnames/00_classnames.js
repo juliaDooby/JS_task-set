5fc99f1426359e61fffea1af9ee73d033b605a35
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-01 20:51:54 +0300

    Update 00_classnames.js

--- Subject/React_classnames/00_classnames.js
+++ Subject/React_classnames/00_classnames.js
@@ -1,1 +1,20 @@
+---------------------------------------------------------------------------------------------
+⋙ ❍ React classnames:
+---
+0. Простое использование `classnames`
+⌛ Задача: Добавьте класс `active` к компоненту, если передан пропс `isActive`.
+🎯 Решение:
+```javascript
+import classNames from 'classnames';
 
+const Button = ({ isActive }) => {
+  const buttonClass = classNames({ active: isActive });
+
+  return <button className={buttonClass}>Click me</button>;
+};
+
+// Использование
+<Button isActive={true} />; // <button class="active">Click me</button>
+```
+Объяснение: Если `isActive` равен `true`, класс `active` будет добавлен.
+---------------------------------------------------------------------------------------------
