ad5a69013e53a8b99d485437aa0afc578c7f8205
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-01 20:55:32 +0300

    Update 06_classnames.js

--- Subject/React_classnames/06_classnames.js
+++ Subject/React_classnames/06_classnames.js
@@ -1,1 +1,18 @@
+---------------------------------------------------------------------------------------------
+⋙ ❍ React classnames:
+---
+6. Динамическая передача классов
+⌛ Задача: Добавьте дополнительные классы через проп `className`.
+🎯 Решение:
+```javascript
+const Button = ({ isPrimary, className }) => {
+  const buttonClass = classNames('button', { primary: isPrimary }, className);
 
+  return <button className={buttonClass}>Click me</button>;
+};
+
+// Использование
+<Button isPrimary={true} className="custom-class" />; // <button class="button primary custom-class">Click me</button>
+```
+Объяснение: Вы можете комбинировать проп `className` с динамическими классами.
+---------------------------------------------------------------------------------------------
