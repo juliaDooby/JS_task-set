4ae120fb032bbb33264c5e936759cc08cbac32ed
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-01 20:30:41 +0300

    Update 13_styled.js

--- Subject/React_styled-components/13_styled.js
+++ Subject/React_styled-components/13_styled.js
@@ -1,1 +1,29 @@
+---------------------------------------------------------------------------------------------
+⋙ ❍ styled-components:
+---
+13. Тематизация с `ThemeProvider`
+⌛ Задача: Используйте тему для управления цветами.
+🎯 Решение:
+```javascript
+import { ThemeProvider } from 'styled-components';
 
+const theme = {
+  primary: 'blue',
+  secondary: 'green',
+};
+
+const Button = styled.button`
+  background-color: ${props => props.theme.primary};
+  color: white;
+`;
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <Button>Click me</Button>
+  </ThemeProvider>
+);
+
+// Кнопка будет синей, используя тему.
+```
+Объяснение: `ThemeProvider` позволяет управлять стилями через тему.
+---------------------------------------------------------------------------------------------
