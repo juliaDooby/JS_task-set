37f5f9f93533d33c0cffa0e93ce951a15f157ff6
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-01 20:58:12 +0300

    Update 14_classnames.js

--- Subject/React_classnames/14_classnames.js
+++ Subject/React_classnames/14_classnames.js
@@ -1,1 +1,17 @@
+---------------------------------------------------------------------------------------------
+⋙ ❍ React classnames:
+---
+14. Комбинирование классов через пропы
+⌛ Задача: Добавьте пользовательский класс через проп `extraClass`.
+🎯 Решение:
+```javascript
+const Button = ({ extraClass }) => {
+  const buttonClass = classNames('button', extraClass);
 
+  return <button className={buttonClass}>Click me</button>;
+};
+
+// Использование
+<Button extraClass="my-custom-class" />; // <button class="button my-custom-class">Click me</button>
+```
+---------------------------------------------------------------------------------------------
