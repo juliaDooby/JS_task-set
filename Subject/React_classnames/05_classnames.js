73a0dde7fdfe70e90e36c4bcb5320339745ff8b7
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-01 20:54:52 +0300

    Update 05_classnames.js

--- Subject/React_classnames/05_classnames.js
+++ Subject/React_classnames/05_classnames.js
@@ -1,1 +1,21 @@
+---------------------------------------------------------------------------------------------
+⋙ ❍ React classnames:
+---
+5. Комбинация строк, массивов и объектов
+⌛ Задача: Объедините все способы указания классов.
+🎯 Решение:
+```javascript
+const Button = ({ isPrimary, isDisabled }) => {
+  const buttonClass = classNames('button', ['extra-class'], {
+    'btn-primary': isPrimary,
+    'btn-disabled': isDisabled,
+  });
 
+  return <button className={buttonClass}>Click me</button>;
+};
+
+// Использование
+<Button isPrimary={true} isDisabled={false} />; // <button class="button extra-class btn-primary">Click me</button>
+```
+Объяснение: `classnames` позволяет смешивать строки, массивы и объекты в одном вызове.
+---------------------------------------------------------------------------------------------
