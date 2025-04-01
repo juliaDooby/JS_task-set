0d36e09395ec4cfd2cbfc48b3bae990299251077
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-01 21:30:19 +0300

    Update 14_async-await.js

--- Subject/Async.Await/14_async-await.js
+++ Subject/Async.Await/14_async-await.js
@@ -1,1 +1,26 @@
+---------------------------------------------------------------------------------------------
+⋙ ❍ async/await:
+---
+14. Промисификация функций
+⌛ Задача: Превратите функцию с колбэком в `async/await`.
+🎯 Решение:
+```javascript
+function delay(ms, callback) {
+  setTimeout(() => callback(null, "Done"), ms);
+}
 
+function promisify(fn) {
+  return function(...args) {
+    return new Promise((resolve, reject) => {
+      fn(...args, (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+  };
+}
+
+const delayAsync = promisify(delay);
+delayAsync(1000).then(console.log); // "Done"
+```
+---------------------------------------------------------------------------------------------
