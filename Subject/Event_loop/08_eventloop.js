3cd54164475a8c1670ff7181190995a89037be02
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-03 10:46:58 +0300

    Update 08_eventloop.js

--- Subject/Event_loop/08_eventloop.js
+++ Subject/Event_loop/08_eventloop.js
@@ -1,1 +1,18 @@
+---------------------------------------------------------------------------------------------  
+⋙ ❍ Event Loop:
+--- 
+8. Рекурсивный `setTimeout`
+⌛ Задача: Реализуйте интервал с использованием `setTimeout`.
+🎯 Решение:
+```javascript
+function customInterval(callback, delay) {
+  function execute() {
+    callback();
+    setTimeout(execute, delay);
+  }
+  setTimeout(execute, delay);
+}
 
+customInterval(() => console.log("Interval"), 1000);
+```
+---------------------------------------------------------------------------------------------
