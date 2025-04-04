78c74953a7d23d6cbc2e862a4ebed209cac6abc6
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-04 11:06:38 +0300

    Update 14_ES6+.js

--- Subject/Modern_standard-ES6+/14_ES6+.js
+++ Subject/Modern_standard-ES6+/14_ES6+.js
@@ -1,1 +1,14 @@
+---------------------------------------------------------------------------------------------
+⋙ ❍ Современные стандарты JavaScript (ES6+):
+---
+14. `Object.entries` и `Object.fromEntries`
+Методы `Object.entries` и `Object.fromEntries` позволяют работать с парами ключ-значение объектов.
+```javascript
+const obj = { a: 1, b: 2 };
+const entries = Object.entries(obj);
+console.log(entries); // [['a', 1], ['b', 2]]
 
+const newObj = Object.fromEntries(entries);
+console.log(newObj); // { a: 1, b: 2 }
+```
+---------------------------------------------------------------------------------------------
