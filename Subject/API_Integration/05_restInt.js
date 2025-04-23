927433fcf0c9b52b6db8d19598b537bd8ba3a3ac
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-23 15:25:16 +0300

    Update 05_restInt.js

--- Subject/API_Integration/05_restInt.js
+++ Subject/API_Integration/05_restInt.js
@@ -1,1 +1,30 @@
+---------------------------------------------------------------------------------------------  
+⋙ ❍ Интеграция:
+---
+5. Интеграция с Twilio (отправка SMS)
+⌛ Задача: Используйте Twilio API для отправки SMS.
+🎯 Решение:
+1. Установите Twilio SDK:
+```bash
+npm install twilio
+```
+2. Реализуйте код для отправки SMS:
+```javascript
+const twilio = require('twilio');
 
+const client = new twilio('YOUR_ACCOUNT_SID', 'YOUR_AUTH_TOKEN');
+
+function sendSMS(to, body) {
+  client.messages.create({
+    body: body,
+    from: 'YOUR_TWILIO_PHONE_NUMBER',
+    to: to
+  })
+  .then(message => console.log(message.sid))
+  .catch(error => console.error(error));
+}
+
+sendSMS('+1234567890', 'Hello, this is a test message!');
+```
+Объяснение: Используем Twilio для отправки SMS через API. Для работы необходимо иметь аккаунт в Twilio и ключи доступа.
+---------------------------------------------------------------------------------------------  
