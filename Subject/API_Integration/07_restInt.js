3e3847f9d6e79437d88843ddf620c9e78a68cfef
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-23 15:25:39 +0300

    Update 07_restInt.js

--- Subject/API_Integration/07_restInt.js
+++ Subject/API_Integration/07_restInt.js
@@ -1,1 +1,27 @@
+---------------------------------------------------------------------------------------------  
+⋙ ❍ Интеграция:
+---
+7. Интеграция с GraphQL API
+⌛ Задача: Напишите запрос к GraphQL API.
+🎯 Решение:
+```javascript
+const query = `
+  query {
+    posts {
+      title
+      body
+    }
+  }
+`;
 
+fetch('https://graphql.example.com', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ query: query })
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
+```
+Объяснение: Мы выполняем запрос GraphQL с помощью `fetch`, отправляя строку запроса в теле POST-запроса.
+---------------------------------------------------------------------------------------------  
