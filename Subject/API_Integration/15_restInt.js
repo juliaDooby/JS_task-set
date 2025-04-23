3b0cde2d49f920e3b683f3ed7ae12b09e0f7eee5
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-23 15:27:29 +0300

    Update 15_restInt.js

--- Subject/API_Integration/15_restInt.js
+++ Subject/API_Integration/15_restInt.js
@@ -1,1 +1,24 @@
+---------------------------------------------------------------------------------------------  
+⋙ ❍ Интеграция:
+---
+15. Интеграция с Algolia для поиска
+⌛ Задача: Настройте поисковую систему с помощью Algolia.
+🎯 Решение:
+```bash
+npm install algoliasearch
+```
 
+```javascript
+import algoliasearch from 'algoliasearch';
+
+const client = algoliasearch('YOUR_APPLICATION_ID', 'YOUR_SEARCH_ONLY_API_KEY');
+const index = client.initIndex('your_index_name');
+
+index.search('query')
+  .then(({ hits }) => {
+    console.log(hits);
+  })
+  .catch(err => console.log(err));
+```
+Объяснение: Используем Algolia для выполнения поиска по индексу.
+---------------------------------------------------------------------------------------------  
