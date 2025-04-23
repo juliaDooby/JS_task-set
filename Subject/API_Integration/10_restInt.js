82e9c5fcf8c8e29c72cd6a74e9d01ec945b13d89
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-23 15:26:24 +0300

    Update 10_restInt.js

--- Subject/API_Integration/10_restInt.js
+++ Subject/API_Integration/10_restInt.js
@@ -1,1 +1,38 @@
+---------------------------------------------------------------------------------------------  
+⋙ ❍ Интеграция:
+---
+10. Интеграция с AWS S3 для загрузки файлов
+⌛ Задача: Напишите код для загрузки файла в S3 с использованием AWS SDK.
+🎯 Решение:
+```javascript
+import AWS from 'aws-sdk';
 
+AWS.config.update({
+  accessKeyId: 'YOUR_ACCESS_KEY',
+  secretAccessKey: 'YOUR_SECRET_KEY',
+  region: 'us-east-1'
+});
+
+const s3 = new AWS.S3();
+
+function uploadFile(file) {
+  const params = {
+    Bucket: 'your-bucket-name',
+    Key: file.name,
+    Body: file,
+    ACL: 'public-read'
+  };
+
+  s3.upload(params, function(err, data) {
+    if (err) {
+      console.error('Error uploading file:', err);
+    } else {
+      console.log('File uploaded successfully:', data.Location);
+    }
+  });
+}
+
+uploadFile(fileInput.files[0]); // Example of uploading file from an input field
+```
+Объяснение: Этот код использует AWS SDK для загрузки файла в S3. Мы передаём файл и параметры для загрузки.
+---------------------------------------------------------------------------------------------  
