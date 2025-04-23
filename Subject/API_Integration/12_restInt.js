9fc76764a9a75b6ece90ddede01a8a8680d8a4ac
Julia: @juliaDooby <juliadooby@yandex.ru>
2025-04-23 15:26:50 +0300

    Update 12_restInt.js

--- Subject/API_Integration/12_restInt.js
+++ Subject/API_Integration/12_restInt.js
@@ -1,1 +1,32 @@
+---------------------------------------------------------------------------------------------  
+⋙ ❍ Интеграция:
+---
+12. Интеграция с Firebase Realtime Database
+⌛ Задача: Напишите код для взаимодействия с Firebase Realtime Database.
+🎯 Решение:
+```javascript
+import firebase from 'firebase/app';
+import 'firebase/database';
 
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN',
+  databaseURL: 'YOUR_DATABASE_URL',
+  projectId: 'YOUR_PROJECT_ID',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+function writeData() {
+  database.ref('users/1').set({
+    name: 'John',
+    age: 30
+  });
+}
+
+writeData();
+```
+Объяснение: Мы подключаем Firebase и используем его Realtime Database для записи данных.
+---------------------------------------------------------------------------------------------  
